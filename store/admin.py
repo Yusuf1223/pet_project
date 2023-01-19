a836930504ca435edbf8588c9070a56ec3716c24
Yusuf1223 <josephrb@yandex.com>
2023-01-19 16:57:47 +0500

    1. Add new Models for Watch and register them into admin 2. Add Image and WatchImage Models and register

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from store.models import Feedback, Info, Contact, Right
+from store.models import *
 # Register your models here.
 
 admin.site.register(Feedback)
 admin.site.register(Info)
 admin.site.register(Contact)
 admin.site.register(Right)
+
+admin.site.register(Color)
+admin.site.register(Material)
+admin.site.register(WatchStyle)
+admin.site.register(Coating)
+admin.site.register(GlassMaterial)
+admin.site.register(Watch)
+admin.site.register(Image)
+admin.site.register(WatchImage)
+
+
+
+
+
--- store/models.py
+++ store/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MinLengthValidator
 # Models for information about company and feedback from clients model
 class Feedback(models.Model):
     name = models.CharField(max_length=128,
-                            validators=[MinLengthValidator(2, "Name should be longer than 2 characters")])
+                            validators=[MinLengthValidator(2, "Name must have more than 2 characters")])
 
     number = models.CharField(max_length=16)
     email = models.EmailField()
@@ -17,7 +17,7 @@ class Feedback(models.Model):
 
 class Info(models.Model):
     title = models.CharField(max_length=128,
-                             validators=[MinLengthValidator(2, "Title should be longer than 2 characters")])
+                             validators=[MinLengthValidator(2, "Title must have more than 2 characters")])
     text = models.TextField()
     key = models.CharField(max_length=128)  # it determines which of the pages we are in
 
@@ -38,3 +38,62 @@ class Right(models.Model):
     legal_address = models.CharField(max_length=128)
     email = models.EmailField()
 
+
+# Watch Model and Models for Filtering
+
+class Color(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class Material(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class WatchStyle(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class Coating(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class GlassMaterial(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class Watch(models.Model):
+    title = models.CharField(max_length=128,
+                             validators=[MinLengthValidator(2, "Title must have more than 2 characters")])
+    article = models.CharField(max_length=128,
+                               validators=[MinLengthValidator(2, "Title must have more than 2 characters")])
+    watch_size = models.IntegerField()
+
+    watch_color = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True, related_name='watch_color')
+    watch_material = models.ForeignKey('Material', on_delete=models.SET_NULL, null=True, related_name='watch_material')
+    bracelet_size = models.IntegerField()
+
+    bracelet_color = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True, related_name='bracelet_color')
+    bracelet_material = models.ForeignKey('Material', on_delete=models.SET_NULL, null=True, related_name='bracelet_material')
+    glass_material = models.ForeignKey('GlassMaterial', on_delete=models.SET_NULL, null=True)
+
+    style = models.ForeignKey('WatchStyle', on_delete=models.SET_NULL, null=True)
+    coating = models.ForeignKey('Coating', on_delete=models.SET_NULL, null=True)
+
+    water_resistance = models.IntegerField()
+    price = models.FloatField()
+    count = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# Image Model
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='static/img')
+
+
+class WatchImage(Image):
+    watch = models.ForeignKey('Watch', on_delete=models.CASCADE)
